18f3033a2745ce8569b514399454cd82cedbcac5
Jayesh Mardiya <mardiyajayesh@gmail.com>
2022-02-09 14:57:35 +0530

    setup scripts as Márton B. publishing example(stream-chat-android-main)

--- mathutillibrary/src/main/java/com/uttam/mathutillibrary/BasicMaths.java
+++ mathutillibrary/src/main/java/com/uttam/mathutillibrary/BasicMaths.java
@@ -5,7 +5,6 @@ import android.widget.Toast;
 
 public class BasicMaths {
 
-
     public void showTotal(Context context, int num1, int num2){
         String total = String.valueOf(num1+num2);
         Toast.makeText(context, total, Toast.LENGTH_SHORT).show();
